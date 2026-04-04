157b5f7f4b66fa78dfdbc9a8b763a92fe6297611
Sibin Grasic <sibin.grasic@oblak.studio>
2026-04-04 21:29:59 +0200

    test: add wordpress phpunit harness

---
+++ tests/wp-plugin/xwc-data-type-tests/xwc-data-type-tests.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: XWC Data Type Test Bootstrap
+ * Description: Test-only plugin wrapper for the XWC data type package.
+ */
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
